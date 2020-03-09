367ab1c4320dcbfe6dd13e32adb5722548ba6590
jpennington2472 <60485338+jpennington2472@users.noreply.github.com>
2020-03-09 18:08:39 -0400

    Added our final changes
    Endpoints working with Database.

--- Interview/PersonsService/src/main/java/com/example/controller/PersonController.java
+++ Interview/PersonsService/src/main/java/com/example/controller/PersonController.java
@@ -1,5 +1,4 @@
 package com.example.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,34 +6,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.example.model.Person;
+import org.springframework.web.client.RestTemplate;import com.example.model.Person;
 import com.example.service.PersonService;
 
 @RestController
-@RequestMapping("/")
-public class PersonController {
-
+@RequestMapping("/person")
+public class PersonController {	
 	@Autowired
-	private PersonService service;
-	
-	
+	private PersonService service;	
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@GetMapping("/person/{name}")
+	private RestTemplate restTemplate;	
+	@GetMapping("/{name}")
 	public Person getPersonByName(@PathVariable String name) {
-		//Person person = restTemplate.getForObject("/http://localhost:9001/person/{name}" + Person.class);
-		
-		System.out.println("get person by name");
-		return  new Person(1,name,29);
-		//return service.getPersonByName(name);
-	}
-	
-	@PostMapping("/person/addperson")
-	public void addPerson(@RequestBody Person person) {
-			service.addPerson(person); 
+		//Person person = restTemplate.getForObject("/http://localhost:9001/person/{name}" + Person.class);		System.out.println("get person by name");
+		//return  new Person(1,name,29);
+		return service.getPersonByName(name);
+	}	
+	@PostMapping("/addperson")
+	public  @ResponseBody Person addPerson(@RequestBody Person person) {
+			service.addPerson(person);
+			return person;
 	}
 }
